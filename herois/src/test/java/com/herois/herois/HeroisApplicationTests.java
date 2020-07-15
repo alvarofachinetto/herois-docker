9716772197e099d789737b5fdddf41bd4ad58016
Alvaro Silva <alvaro.facneto@gmail.com>
2020-07-15 19:32:11 -0300

    Iniciando e configurando projeto

---
+++ herois/src/test/java/com/herois/herois/HeroisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herois.herois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
